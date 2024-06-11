31192146e59638c3eab1bee94495d9d1de272b53
hazhalim <22002548@siswa.um.edu.my>
2024-06-11 12:05:35 +0800

    Updated backend, progress on implementation of authentication

--- src/main/resources/initialise_egringotts_database.sql
+++ src/main/resources/initialise_egringotts_database.sql
@@ -1,6 +1,4 @@
--- Initialise E-Gringotts Database
--- NOTE: You ONLY have to run these commands: "CREATE SCHEMA egringotts;" AND "USE egringotts;"!
--- The rest will be taken care of by the Java Spring Boot application.
+-- MySQL Workbench Forward Engineering
 
 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
@@ -32,18 +30,6 @@ CREATE TABLE IF NOT EXISTS `egringotts`.`security_question` (
 
 
 -- -----------------------------------------------------
--- Table `egringotts`.`user_image`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `egringotts`.`user_image` (
-                                                         `user_image_id` BIGINT NOT NULL AUTO_INCREMENT,
-                                                         `image_path` VARCHAR(255) NULL DEFAULT NULL,
-                                                         PRIMARY KEY (`user_image_id`))
-    ENGINE = InnoDB
-    DEFAULT CHARACTER SET = utf8mb4
-    COLLATE = utf8mb4_0900_ai_ci;
-
-
--- -----------------------------------------------------
 -- Table `egringotts`.`user`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `egringotts`.`user` (
@@ -67,7 +53,6 @@ CREATE TABLE IF NOT EXISTS `egringotts`.`address` (
                                                       `street_name_1` VARCHAR(255) NULL DEFAULT NULL,
                                                       `street_name_2` VARCHAR(255) NULL DEFAULT NULL,
                                                       `town` VARCHAR(255) NULL DEFAULT NULL,
-                                                      `type` VARCHAR(255) NULL DEFAULT NULL,
                                                       PRIMARY KEY (`address_id`))
     ENGINE = InnoDB
     DEFAULT CHARACTER SET = utf8mb4
@@ -87,24 +72,19 @@ CREATE TABLE IF NOT EXISTS `egringotts`.`account` (
                                                       `gender` VARCHAR(255) NULL DEFAULT NULL,
                                                       `last_login_date` DATETIME(6) NULL DEFAULT NULL,
                                                       `password` VARCHAR(255) NULL DEFAULT NULL,
-                                                      `security_pin` INT NULL DEFAULT NULL,
+                                                      `security_pin` VARCHAR(255) NULL DEFAULT NULL,
                                                       `telephone_number` VARCHAR(255) NULL DEFAULT NULL,
                                                       `username` VARCHAR(255) NULL DEFAULT NULL,
                                                       `address_id` BIGINT NULL DEFAULT NULL,
                                                       `security_question_id` BIGINT NULL DEFAULT NULL,
                                                       `user_id` BIGINT NULL DEFAULT NULL,
-                                                      `user_image_id` BIGINT NULL DEFAULT NULL,
                                                       PRIMARY KEY (`account_id`),
                                                       UNIQUE INDEX `UKq4mt85fqye0pwgamg8vlw8mrb` (`address_id` ASC) VISIBLE,
                                                       UNIQUE INDEX `UKh6dr47em6vg85yuwt4e2roca4` (`user_id` ASC) VISIBLE,
-                                                      UNIQUE INDEX `UKnmpov5sg0inlsc5cen86nonsw` (`user_image_id` ASC) VISIBLE,
                                                       INDEX `FK5c7qbhfu3c0xl8kesk2gq4x78` (`security_question_id` ASC) VISIBLE,
                                                       CONSTRAINT `FK5c7qbhfu3c0xl8kesk2gq4x78`
                                                           FOREIGN KEY (`security_question_id`)
                                                               REFERENCES `egringotts`.`security_question` (`security_question_id`),
-                                                      CONSTRAINT `FK5kqhbp0algux19yi50k3egksc`
-                                                          FOREIGN KEY (`user_image_id`)
-                                                              REFERENCES `egringotts`.`user_image` (`user_image_id`),
                                                       CONSTRAINT `FK7m8ru44m93ukyb61dfxw0apf6`
                                                           FOREIGN KEY (`user_id`)
                                                               REFERENCES `egringotts`.`user` (`user_id`),
@@ -117,15 +97,32 @@ CREATE TABLE IF NOT EXISTS `egringotts`.`account` (
 
 
 -- -----------------------------------------------------
+-- Table `egringotts`.`currency`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `egringotts`.`currency` (
+                                                       `currency_id` BIGINT NOT NULL AUTO_INCREMENT,
+                                                       `abbreviation` VARCHAR(255) NULL DEFAULT NULL,
+                                                       `name` VARCHAR(255) NULL DEFAULT NULL,
+                                                       PRIMARY KEY (`currency_id`))
+    ENGINE = InnoDB
+    DEFAULT CHARACTER SET = utf8mb4
+    COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
 -- Table `egringotts`.`balance`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `egringotts`.`balance` (
                                                       `balance_id` BIGINT NOT NULL AUTO_INCREMENT,
                                                       `balance` DOUBLE NULL DEFAULT NULL,
-                                                      `currency` VARCHAR(255) NULL DEFAULT NULL,
                                                       `account_id` BIGINT NULL DEFAULT NULL,
+                                                      `currency_id` BIGINT NULL DEFAULT NULL,
                                                       PRIMARY KEY (`balance_id`),
                                                       INDEX `FKxhe1wv9gspc3wl0w66bptmgs` (`account_id` ASC) VISIBLE,
+                                                      INDEX `FK45r0l2sc0cdja19no0orl9i95` (`currency_id` ASC) VISIBLE,
+                                                      CONSTRAINT `FK45r0l2sc0cdja19no0orl9i95`
+                                                          FOREIGN KEY (`currency_id`)
+                                                              REFERENCES `egringotts`.`currency` (`currency_id`),
                                                       CONSTRAINT `FKxhe1wv9gspc3wl0w66bptmgs`
                                                           FOREIGN KEY (`account_id`)
                                                               REFERENCES `egringotts`.`account` (`account_id`))
@@ -142,12 +139,36 @@ CREATE TABLE IF NOT EXISTS `egringotts`.`card` (
                                                    `card_number` VARCHAR(255) NULL DEFAULT NULL,
                                                    `cvv` VARCHAR(255) NULL DEFAULT NULL,
                                                    `expiry_date` DATETIME(6) NULL DEFAULT NULL,
+                                                   `type` VARCHAR(255) NULL DEFAULT NULL,
                                                    `account_id` BIGINT NULL DEFAULT NULL,
                                                    PRIMARY KEY (`card_id`),
                                                    INDEX `FK8v67eys6tqflsm6hrdgru2phu` (`account_id` ASC) VISIBLE,
                                                    CONSTRAINT `FK8v67eys6tqflsm6hrdgru2phu`
                                                        FOREIGN KEY (`account_id`)
                                                            REFERENCES `egringotts`.`account` (`account_id`))
+    ENGINE = InnoDB
+    DEFAULT CHARACTER SET = utf8mb4
+    COLLATE = utf8mb4_0900_ai_ci;
+
+
+-- -----------------------------------------------------
+-- Table `egringotts`.`currency_exchange`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `egringotts`.`currency_exchange` (
+                                                                `currency_exchange_rate_id` BIGINT NOT NULL AUTO_INCREMENT,
+                                                                `processing_fee` DOUBLE NULL DEFAULT NULL,
+                                                                `rate` DOUBLE NULL DEFAULT NULL,
+                                                                `from_currency_id` BIGINT NULL DEFAULT NULL,
+                                                                `to_currency_id` BIGINT NULL DEFAULT NULL,
+                                                                PRIMARY KEY (`currency_exchange_rate_id`),
+                                                                INDEX `FK7v1dcryjsgv54jy3hf3s9ug7e` (`from_currency_id` ASC) VISIBLE,
+                                                                INDEX `FKpf6um4qb983kv3hyjyxajr2w7` (`to_currency_id` ASC) VISIBLE,
+                                                                CONSTRAINT `FK7v1dcryjsgv54jy3hf3s9ug7e`
+                                                                    FOREIGN KEY (`from_currency_id`)
+                                                                        REFERENCES `egringotts`.`currency` (`currency_id`),
+                                                                CONSTRAINT `FKpf6um4qb983kv3hyjyxajr2w7`
+                                                                    FOREIGN KEY (`to_currency_id`)
+                                                                        REFERENCES `egringotts`.`currency` (`currency_id`))
     ENGINE = InnoDB
     DEFAULT CHARACTER SET = utf8mb4
     COLLATE = utf8mb4_0900_ai_ci;
@@ -182,14 +203,31 @@ CREATE TABLE IF NOT EXISTS `egringotts`.`transaction` (
                                                           `transaction_id` BIGINT NOT NULL AUTO_INCREMENT,
                                                           `amount` DOUBLE NULL DEFAULT NULL,
                                                           `category` VARCHAR(255) NULL DEFAULT NULL,
-                                                          `currency` VARCHAR(255) NULL DEFAULT NULL,
                                                           `date` DATETIME(6) NULL DEFAULT NULL,
-                                                          `from_account_balance` DOUBLE NULL DEFAULT NULL,
+                                                          `description` VARCHAR(255) NULL DEFAULT NULL,
+                                                          `payment_method` VARCHAR(255) NULL DEFAULT NULL,
+                                                          `receipt_file_path` VARCHAR(255) NULL DEFAULT NULL,
+                                                          `type` VARCHAR(255) NULL DEFAULT NULL,
+                                                          `card_id` BIGINT NULL DEFAULT NULL,
+                                                          `currency_id` BIGINT NULL DEFAULT NULL,
                                                           `from_account_id` BIGINT NULL DEFAULT NULL,
+                                                          `from_account_balance_id` BIGINT NULL DEFAULT NULL,
                                                           `to_account_id` BIGINT NULL DEFAULT NULL,
                                                           PRIMARY KEY (`transaction_id`),
+                                                          UNIQUE INDEX `UKbyn371gf817ljah73cb05vsav` (`card_id` ASC) VISIBLE,
+                                                          INDEX `FKlcx7g8g7x4fyns9k6vesu3n9n` (`currency_id` ASC) VISIBLE,
                                                           INDEX `FKrff4jlxetafju1e5cks5mfcnk` (`from_account_id` ASC) VISIBLE,
+                                                          INDEX `FK3hkkcim0w83trc8nd2aqyorlc` (`from_account_balance_id` ASC) VISIBLE,
                                                           INDEX `FKluqt8k2pa8d4gmggx4rhl5vgv` (`to_account_id` ASC) VISIBLE,
+                                                          CONSTRAINT `FK3hkkcim0w83trc8nd2aqyorlc`
+                                                              FOREIGN KEY (`from_account_balance_id`)
+                                                                  REFERENCES `egringotts`.`balance` (`balance_id`),
+                                                          CONSTRAINT `FK484i2t8acnct6xy8ylevl40go`
+                                                              FOREIGN KEY (`card_id`)
+                                                                  REFERENCES `egringotts`.`card` (`card_id`),
+                                                          CONSTRAINT `FKlcx7g8g7x4fyns9k6vesu3n9n`
+                                                              FOREIGN KEY (`currency_id`)
+                                                                  REFERENCES `egringotts`.`currency` (`currency_id`),
                                                           CONSTRAINT `FKluqt8k2pa8d4gmggx4rhl5vgv`
                                                               FOREIGN KEY (`to_account_id`)
                                                                   REFERENCES `egringotts`.`account` (`account_id`),
